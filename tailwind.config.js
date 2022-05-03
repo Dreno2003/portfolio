f969abd9cd7b34995ffe9d9ecb1adf523b87ed35
Idorenyin Akaininyene <=>
2022-05-03 12:29:17 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    content: ["./public/**/*.{html,js}"],
+    theme: {
+        extend: {
+            colors: {
+                'primary': {
+                    100: '#0FA5E9',
+                    200: '#0B1120', //bg color
+                    300: '#1e293b', //card color    
+                    400: '#10172a'
+                },
+                'deviconcolor': {
+                    100: 'rgba(227, 31, 113, 0.2)',
+                    200: 'rgba(136, 56, 180, 0.2)',
+                    300: '#e0e016',
+                    400: 'rgb(172 187 48 / 20%)'
+                }
+            }
+        },
+    },
+    plugins: [],
+}
